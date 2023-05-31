9ed1fda0e94e629231336d16f567ee7e2c03b44a
surjyakantajana <surjya.wgt@gmail.com>
2023-05-31 19:27:17 +0530

    nothing

--- resources/views/admin/location/countries/show.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 m-t-10">
-    <p>{!! $data->short_description !!}</p>
-    <p>{!! $data->long_description !!}</p>
-</div>
--- resources/views/welcome.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('front.layouts.app')
-@section('content')
-    <div class="content">
-        <div class="title m-b-md">
-            Laravel
-        </div>
-    </div>
-@endsection
